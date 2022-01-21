f4abfd648a9d565659df30f94081c90b92bb8413
tianhe <9932705+zhuyiwenyuntianhe@user.noreply.gitee.com>
2022-01-21 10:14:52 +0800

    完成登录和注册的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,5 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  options.url = 'http://www.liulongbin.top:3007' + options.url
 })
